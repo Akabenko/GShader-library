eea95c2aa6eca4ed87620579d7cc656e64237c6f
Akabenko <danlimons322@gmail.com>
2026-01-09 16:13:45 +0300

    Removed some fixes of MW base, VManip, etc.
    Thats why this addons works good by native with Resolved full frame depth buffer.

--- lua/autorun/client/fixes_for_workshop_addons.lua
+++ lua/autorun/client/fixes_for_workshop_addons.lua
@@ -15,7 +15,7 @@ local SetRenderOrigin = entMeta.SetRenderOrigin
 local SetRenderAngles = entMeta.SetRenderAngles
 local Angle = Angle
 local GetAngles = entMeta.GetAngles
-
+local render = render
 
 hook.Add("LocalPlayer_Validated", libname, function() // First-Person Body
     timer.Simple(0, function()
@@ -32,39 +32,77 @@ hook.Add("LocalPlayer_Validated", libname, function() // First-Person Body
 end)
 
 hook.Add("InitPostEntity", libname, function() 
-    if MW_ATTS then // MW base
-        local function FixMWBase(class)
-            class = class or "mg_base"
+    /*if g_Legs then // GMOD Legs 3
+        old_LegsDoFinalRender = old_LegsDoFinalRender or g_Legs.DoFinalRender
+
+        local rt_blacklist = {
+            ["_rt_waterreflection"] = true,
+            ["_rt_waterrefraction"] = true,
+            ["_rt_camera"] = true,
+        }
+
+        local function HideLegs(ent)
+            SetNoDraw(ent, true)
+            SetRenderOrigin(ent)
+            SetRenderAngles(ent)
+        end
+
+        function g_Legs:DoFinalRender()
+            local rt = render.GetRenderTarget()
+
+            if rt and rt:GetName() then
+                if rt_blacklist[rt:GetName()] then 
+                    return
+                end
+            end
+            
+            --local dist = LocalPlayer():EyePos():DistToSqr(EyePos())
+
+            --[[if dist > 0 then // fix third person render
+                HideLegs(self.LegEnt)
+                return
+            end]]
+
+            SetNoDraw(self.LegEnt, false) // fix depth buffer
+            old_LegsDoFinalRender(self)
+            SetRenderOrigin(self.LegEnt, self.RenderPos)
+            SetRenderAngles(self.LegEnt, self.RenderAngle)
+        end
+    end*/
+
+    if ArcCW then
+        local function FixACRCWBase(class)
+            class = class or "arccw_base"
             local ENT = weapons.GetStored(class)
 
-            function ENT:PostDrawViewModel( ViewModel )
-                local children = GetChildren(self)
-                local ent = children[#children] --mg_viewmodel
-                if !IsValid(ent) then return end
-                SetNoDraw(ent, false)
+            old_DrawHolosight = old_DrawHolosight or ENT.DrawHolosight
+            old_FormCheapScope = old_FormCheapScope or ENT.FormCheapScope
+
+            local function depthBufferCheck()
+                local rt = render.GetRenderTarget()
+                return rt and rt:GetName() == "_rt_resolvedfullframedepth"
+            end
+
+            function ENT:DrawHolosight(...)
+                if depthBufferCheck() then return end
+                old_DrawHolosight(self, ...)
+            end
+
+            function ENT:FormCheapScope(...)
+                if depthBufferCheck() then return end
+                old_FormCheapScope(self, ...)
             end
 
             weapons.Register(ENT, class)
         end
 
         for k,v in pairs(weapons.GetList()) do
-            if string.find(v.ClassName, "mg_") then
-                FixMWBase(v.ClassName)
+            if v.Base == "arccw_base" then
+                FixACRCWBase(v.ClassName)
             end
         end
 
-        FixMWBase()
-    end
-
-    if g_Legs then // GMOD Legs 3
-        old_LegsDoFinalRender = old_LegsDoFinalRender or g_Legs.DoFinalRender
-
-        function g_Legs:DoFinalRender()
-            SetNoDraw(self.LegEnt, false)
-            old_LegsDoFinalRender(self)
-            SetRenderOrigin(self.LegEnt, self.RenderPos)
-            SetRenderAngles(self.LegEnt, self.RenderAngle)
-        end
+        FixACRCWBase()
     end
 
     if EnhancedCameraTwo then // Enhanced Camera 2
@@ -93,18 +131,58 @@ hook.Add("InitPostEntity", libname, function()
         function EnhancedCameraTwo:Render()
             local renderPos, renderAngle = self:GetRenderPosAngle()
             old_enca2_render(self)
-            SetRenderOrigin(self.entity, renderPos)
-            SetRenderAngles(self.entity, renderAngle)
+            if IsValid(self.entity) then // was lua error sometimes
+                SetRenderOrigin(self.entity, renderPos)
+                SetRenderAngles(self.entity, renderAngle)
+            end
         end
 
         function EnhancedCameraTwo:SetModel(model)
             old_enca2_setmodel(self, model)
-            SetNoDraw(self.entity, false)
+            if IsValid(self.entity) then
+                SetNoDraw(self.entity, false)
+            end
         end
     end
 end)
 
-hook.Add("Initialize", libname,function() // clegs
+hook.Add("Initialize", libname,function()
+    --RunConsoleCommand("mat_viewportscale", 1) -- some users have 0.8 value. so it broke depth buffer
+    hook.Remove("NeedsDepthPass", "FAS2_NeedsDepthPass") // FAS2 fix: Fas2 broke ResolvedDepthBuffer
+    
+    if TFA then
+        -- fix TFA flashlight glitches
+
+        hook.Add("PreDrawTranslucentRenderables", "TFA_fix_flashligth", function(isDrawingDepth, isDrawSkybox, isDraw3DSkybox)
+            if isDrawingDepth or isDrawSkybox or isDraw3DSkybox then return end
+
+            if render.GetViewSetup(true).viewid != 0 then return end
+
+            local wep = LocalPlayer():GetActiveWeapon()
+            if !IsValid(wep) then return end
+            if not wep.IsTFAWeapon then return end
+
+            wep:DrawFlashlight(wep)
+        end)
+
+        local function FixTFABase(class)
+            local ENT = weapons.GetStored(class)
+
+            function ENT:UpdateProjectedTextures(view)
+                self:DrawLaser(view)
+                -- self:DrawFlashlight(view)
+            end
+
+            weapons.Register(ENT, class)
+        end
+
+        for k,v in pairs(weapons.GetList()) do
+            if v.Base == "tfa_gun_base" or v.IsTFAWeapon then
+                FixTFABase(v.ClassName)
+            end
+        end
+    end
+
     if CLegs then
         local ENT = scripted_ents.Get("firstperson_legs")
         ENT.RenderGroup = RENDERGROUP_OPAQUE
@@ -119,29 +197,34 @@ hook.Add("Initialize", libname,function() // clegs
 
         scripted_ents.Register( ENT, "firstperson_legs" )
     end
-
-    if VManip then
-        hook.Add("PostDrawViewModel", libname, function()// 不明原因
-            if IsValid(VManip.VMGesture) and hook.Run("NeedsDepthPass") == true then
-                VManip.VMGesture:SetupBones()
-                VManip.VMGesture:DrawModel()
-            end
-        end)
-    end
 end)
-
 
 /*---------------------------------------------------------------------------
 FIXED:
 First Person Legs:          https://steamcommunity.com/sharedfiles/filedetails/?id=3348203779&searchtext=legs
 Modern Bentleyfare Base:    https://steamcommunity.com/workshop/filedetails/?id=2459720887
-GMOD Legs 3                 https://steamcommunity.com/sharedfiles/filedetails/?id=112806637&searchtext=gmod+legs
+GMOD Legs 3                 https://steamcommunity.com/sharedfiles/filedetails/?id=112806637&searchtext=gmod+legs (Mirror bug)
 clegs                       https://steamcommunity.com/sharedfiles/filedetails/?id=3386878739&searchtext=clegs+
 Enhanced Camera 2           https://steamcommunity.com/sharedfiles/filedetails/?id=2203217139
-VManip (Base)               https://steamcommunity.com/sharedfiles/filedetails/?id=2155366756
+ACRCW write FrameBuffer info DepthBuffer 
 
 UNFIXED:
 Enhanced Camera             https://steamcommunity.com/sharedfiles/filedetails/?id=678037029&searchtext=Enhanced+Camera
+
+CFShadow: эта хуйня походу ломает буфер глубины к хуям
+https://steamcommunity.com/sharedfiles/filedetails/?id=3386876474
+
+Тело от Первого лица: https://steamcommunity.com/sharedfiles/filedetails/?id=3104967124
+
+CONFIRM:
+VManip (Base)               https://steamcommunity.com/sharedfiles/filedetails/?id=2155366756
+
+Исправлено:
+1. В TFA (3D SCOPES) не появляются фонтанчики от выстрелом при включенном MRT
+2. Фонарики ТФА запаздыают из-за буфера глубины (фикс - выбран рекомендованный хук для обновления projtexture)
+4. Антискример не давал вызвать локализированные функции DrawScreenQuad https://steamcommunity.com/sharedfiles/filedetails/?id=3404179264
+
+
 ---------------------------------------------------------------------------*/
 
 
