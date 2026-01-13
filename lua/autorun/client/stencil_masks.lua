d95d4b94ebd190a2dfce8c8e4947ea3a210ce633
Akabenko <danlimons322@gmail.com>
2026-01-13 20:30:05 +0300

    MW Custom Menu adaptation for stencil

--- lua/autorun/client/stencil_masks.lua
+++ lua/autorun/client/stencil_masks.lua
@@ -1,4 +1,3 @@
-
 local stencilHook = "StencilMasks"
 
 STENCIL_WEAPON = 0x01
@@ -7,9 +6,7 @@ hook.Add("PreDrawViewModel", stencilHook, function(vm, ply, weapon, flags)
 	flags = flags or STUDIO_RENDER
 	local isDepthPass = ( bit.band( flags, STUDIO_SSAODEPTHTEXTURE ) != 0 || bit.band( flags, STUDIO_SHADOWDEPTHTEXTURE ) != 0 )
 	if ( isDepthPass ) then return end
-	
-	if IsValid(weapon) and weapon:GetClass() == "gmod_camera" then return end
-	
+
 	render.SetStencilEnable( true )
 
 	render.SetStencilWriteMask(0xFF)
@@ -17,7 +14,8 @@ hook.Add("PreDrawViewModel", stencilHook, function(vm, ply, weapon, flags)
 	render.SetStencilReferenceValue(STENCIL_WEAPON)
 	
 	render.SetStencilCompareFunction(STENCIL_ALWAYS)
-	render.SetStencilPassOperation(STENCIL_REPLACE)
+	-- MW Custom Menu adaptation
+	render.SetStencilPassOperation(IsValid(MW_CUSTOMIZEMENU) and STENCIL_KEEP or STENCIL_REPLACE)
 	render.SetStencilFailOperation(STENCIL_KEEP)
 	render.SetStencilZFailOperation(STENCIL_KEEP)
 end)
