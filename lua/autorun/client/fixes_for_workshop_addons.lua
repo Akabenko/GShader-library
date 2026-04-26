1b857585ca51a7e319f98708d61de5d9cfaeee48
Akabenko <danlimons322@gmail.com>
2026-04-26 18:57:30 +0300

    Update fixes_for_workshop_addons.lua

--- lua/autorun/client/fixes_for_workshop_addons.lua
+++ lua/autorun/client/fixes_for_workshop_addons.lua
@@ -17,7 +17,7 @@ local Angle = Angle
 local GetAngles = entMeta.GetAngles
 local render = render
 
-hook.Add("LocalPlayer_Validated", libname, function() // First-Person Body
+--[[hook.Add("LocalPlayer_Validated", libname, function() // First-Person Body
     timer.Simple(0, function()
         local ply = LocalPlayer()
         local ply_Body = ply.Body
@@ -29,7 +29,7 @@ hook.Add("LocalPlayer_Validated", libname, function() // First-Person Body
             end
         end
     end)
-end)
+end)]]
 
 hook.Add("InitPostEntity", libname, function() 
     /*if g_Legs then // GMOD Legs 3
@@ -70,7 +70,7 @@ hook.Add("InitPostEntity", libname, function()
         end
     end*/
 
-    if ArcCW then
+    if ArcCW then -- ArcCW draw colors to resolved depth buffer
         local function FixACRCWBase(class)
             class = class or "arccw_base"
             local ENT = weapons.GetStored(class)
@@ -105,7 +105,7 @@ hook.Add("InitPostEntity", libname, function()
         FixACRCWBase()
     end
 
-    if EnhancedCameraTwo then // Enhanced Camera 2
+    --[[if EnhancedCameraTwo then // Enhanced Camera 2
         old_enca2_render = old_enca2_render or EnhancedCameraTwo.Render
         old_enca2_setmodel = old_enca2_setmodel or EnhancedCameraTwo.SetModel
 
@@ -143,14 +143,16 @@ hook.Add("InitPostEntity", libname, function()
                 SetNoDraw(self.entity, false)
             end
         end
-    end
+    end]]
 end)
 
 hook.Add("Initialize", libname,function()
     --RunConsoleCommand("mat_viewportscale", 1) -- some users have 0.8 value. so it broke depth buffer
-    hook.Remove("NeedsDepthPass", "FAS2_NeedsDepthPass") // FAS2 fix: Fas2 broke ResolvedDepthBuffer
+    --hook.Remove("NeedsDepthPass", "FAS2_NeedsDepthPass") // FAS2 fix: Fas2 broke ResolvedDepthBuffer
+    RunConsoleCommand("fas2_blureffects", 0)
+    RunConsoleCommand("fas2_blureffects_depth", 0)
     
-    if TFA then
+    --[[if TFA then
         -- fix TFA flashlight glitches
 
         hook.Add("PreDrawTranslucentRenderables", "TFA_fix_flashligth", function(isDrawingDepth, isDrawSkybox, isDraw3DSkybox)
@@ -181,9 +183,9 @@ hook.Add("Initialize", libname,function()
                 FixTFABase(v.ClassName)
             end
         end
-    end
+    end]]
 
-    if CLegs then
+    --[[if CLegs then
         local ENT = scripted_ents.Get("firstperson_legs")
         ENT.RenderGroup = RENDERGROUP_OPAQUE
 
@@ -196,12 +198,13 @@ hook.Add("Initialize", libname,function()
         end
 
         scripted_ents.Register( ENT, "firstperson_legs" )
-    end
+    end]]
 end)
 
 /*---------------------------------------------------------------------------
 FIXED:
 First Person Legs:          https://steamcommunity.com/sharedfiles/filedetails/?id=3348203779&searchtext=legs
+Modern Bentleyfare Base:    https://steamcommunity.com/workshop/filedetails/?id=2459720887
 GMOD Legs 3                 https://steamcommunity.com/sharedfiles/filedetails/?id=112806637&searchtext=gmod+legs (Mirror bug)
 clegs                       https://steamcommunity.com/sharedfiles/filedetails/?id=3386878739&searchtext=clegs+
 Enhanced Camera 2           https://steamcommunity.com/sharedfiles/filedetails/?id=2203217139
@@ -210,19 +213,21 @@ ACRCW write FrameBuffer info DepthBuffer
 UNFIXED:
 Enhanced Camera             https://steamcommunity.com/sharedfiles/filedetails/?id=678037029&searchtext=Enhanced+Camera
 
-CFShadow: эта хуйня походу ломает буфер глубины к хуям
+CFShadow: broke depth buffer
 https://steamcommunity.com/sharedfiles/filedetails/?id=3386876474
 
 Тело от Первого лица: https://steamcommunity.com/sharedfiles/filedetails/?id=3104967124
 
+CONFIRM:
+VManip (Base)               https://steamcommunity.com/sharedfiles/filedetails/?id=2155366756
+
 Исправлено:
 1. В TFA (3D SCOPES) не появляются фонтанчики от выстрелом при включенном MRT
 2. Фонарики ТФА запаздыают из-за буфера глубины (фикс - выбран рекомендованный хук для обновления projtexture)
-3. Антискример не давал вызвать локализированные функции DrawScreenQuad https://steamcommunity.com/sharedfiles/filedetails/?id=3404179264
+4. Антискример не давал вызвать локализированные функции DrawScreenQuad https://steamcommunity.com/sharedfiles/filedetails/?id=3404179264
 
 
 ---------------------------------------------------------------------------*/
-
 
 
 
