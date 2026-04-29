c13943ef45b1c7b7f9784730d3701a99cd22c683
Akabenko <danlimons322@gmail.com>
2026-04-29 15:54:05 +0300

    Update fixes_for_workshop_addons.lua

--- lua/autorun/client/fixes_for_workshop_addons.lua
+++ lua/autorun/client/fixes_for_workshop_addons.lua
@@ -7,9 +7,9 @@ local libname = "shaderlib_fixes"
 
 hook.Add("Initialize", libname,function()
     --RunConsoleCommand("mat_viewportscale", 1) -- some users have 0.8 value. so it broke depth buffer
-    --hook.Remove("NeedsDepthPass", "FAS2_NeedsDepthPass") // FAS2 fix: Fas2 broke ResolvedDepthBuffer
-    RunConsoleCommand("fas2_blureffects", 0)
-    RunConsoleCommand("fas2_blureffects_depth", 0)
+    hook.Remove("NeedsDepthPass", "FAS2_NeedsDepthPass") // FAS2 fix: Fas2 broke ResolvedDepthBuffer
+    --RunConsoleCommand("fas2_blureffects", 0)
+    --RunConsoleCommand("fas2_blureffects_depth", 0)
 end)
 
 /*---------------------------------------------------------------------------
