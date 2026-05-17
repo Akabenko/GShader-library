e30f57a75e4090b2bef394e9c0e24e2fbf089647
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-17 15:07:13 +0300

    Create common_ps_lut.h

---
+++ shadersrc/common_ps_lut.h
@@ -0,0 +1,10 @@
+// Code by LaVashik
+// tex: Data sampler (LUT)
+// coord: Integer coordinates [x, y]
+// texelSize: Texel size [1 / width, 1 / height]
+// return: Raw data (Float32)
+float4 LoadLUT(sampler tex, int2 coord, float2 texelSize)
+{
+    float2 uv = (coord + 0.5) * texelSize;
+    return tex2Dlod(tex, float4(uv, 0, 0));
+}
