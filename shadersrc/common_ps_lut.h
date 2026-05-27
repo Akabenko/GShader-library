17f36fd66edaced095adad916d2399221e64f317
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-27 12:58:54 +0300

    Added 1D LUT reading

--- shadersrc/common_ps_lut.h
+++ shadersrc/common_ps_lut.h
@@ -14,3 +14,9 @@ float4 LoadLUT(sampler2D tex, int2 coord, float2 texelSize)
     float2 uv = (coord + 0.5) * texelSize;
     return tex2Dlod(tex, float4(uv, 0, 0));
 }
+
+float4 LoadLUT(sampler1D tex, int coord, float texelSize)
+{
+    float2 u = (coord + 0.5) * texelSize;
+    return tex1Dlod(tex, float4(u, 0, 0));
+}
